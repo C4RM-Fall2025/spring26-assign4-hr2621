c681e3e5683c2c049040d3d9e9edaffd9fd33712
hr2621 <hr2621@columbia.edu>
2026-02-23 20:15:14 -0500

    Update BondPrice_E_File.py

--- BondPrice_E_File.py
+++ BondPrice_E_File.py
@@ -1,4 +1,17 @@
 
 
 def getBondPrice_E(face, couponRate, yc):
-    return(2098949)
+
+    bondPrice = 0
+    c = face * couponRate
+    
+    for t, rate in enumerate(yc, start = 1):
+
+        cf = c
+        if t == m:
+            cf = cf + face
+
+        pv = cf / ((1 + rate) ** t)
+        bondPrice = bondPrice + pv
+
+    return(bondPrice)
