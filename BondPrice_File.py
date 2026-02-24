9b25a075262c9e6be64b92cf529f13f458376fea
hr2621 <hr2621@columbia.edu>
2026-02-23 20:13:52 -0500

    Update BondPrice_File.py

--- BondPrice_File.py
+++ BondPrice_File.py
@@ -1,8 +1,22 @@
 
 
-def getBondPrice(y, face, couponRate, m, ppy=1):
-    if ppy == 1:
-        x = 2170604
-    if ppy == 2:
-        x = 2171686
-    return(x)
+def getBondPrice(y, face, couponRate, m, ppy = 1):
+
+    n = int(m * ppy)
+    r = y / ppy
+    c = face * couponRate / ppy
+
+    bondPrice = 0
+
+    t = 1
+    while t <= n:
+        cf = c
+        if t == n:
+            cf = cf + face
+
+        pv = cf / ((1 + r) ** t)
+        bondPrice = bondPrice + pv
+
+        t = t + 1
+
+    return(bondPrice)
